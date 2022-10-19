8afaf67af1af1e686e324c2d3282c3d8572e9e21
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-10-19 11:25:47 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@ Config = {}
 
 Config.OwnedResources = {}
 
+-- If you have ESX Legacy 1.8.5 or above, then uncomment the line in fx-manifest.lua.
+-- DONT TOUCH!!! This is automatick
 Config.Framework = ""
 if GetResourceState("qb-core") ~= "missing" then
   Config.Framework = "qb-core"

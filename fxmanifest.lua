9ca86ffa5eac5500f5b544f7403fddfb26b07237
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-10-19 11:24:52 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,13 @@
 fx_version "cerulean"
 game "gta5"
 
+author "Zerio#0880"
+description "Library of functions to allow modification of core functions in my scripts."
+version "1.0.0"
+
 shared_scripts {
+    -- Uncomment if you have ESX Legacy 1.8.5 or above
+    -- "@es_extended/imports.lua",
     "config.lua",
 }
 

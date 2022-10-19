f390c102a9b6cdebd58f723da28744b2d2785352
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-10-19 10:35:44 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean"
+game "gta5"
+
+shared_scripts {
+    "config.lua",
+}
+
+client_scripts {
+    "client/functions.lua",
+    "client/frameworks/*.lua",
+}
+
+server_scripts {
+    "server/functions.lua",
+    "server/frameworks/*.lua",
+}

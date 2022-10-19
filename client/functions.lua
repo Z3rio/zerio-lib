c084169d27f624f25d65bdac4b9c0e362d4a89b3
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-10-19 11:22:02 +0200

    Update functions.lua

--- client/functions.lua
+++ client/functions.lua
@@ -1,3 +1,14 @@
+if Config.Framework == "qb-core" then
+  Framework = exports["qb-core"]:GetCoreObject()
+elseif Config.Framework == "esx" then
+  if ESX then
+    Framework = ESX
+  end
+  else
+    TriggerEvent("esx:getSharedObject", function(obj) Framework = obj end)
+  end
+end
+
 Functions = {
 
 }
